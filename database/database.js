c07afdca73d9c31f6f697d703d8100d2b4bb6d66
SergKnyazev <serg.knyazev77@gmail.com>
2021-09-08 16:26:23 +0300

    testing connect with a locale database of mysql

--- database/database.js
+++ database/database.js
@@ -3,10 +3,18 @@ require('dotenv').config();
 
 const database = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD, {
   dialect: 'mysql',
-  host: 'remotemysql.com',
+  host: '127.0.0.1',
   define: {
     timestamps: false,
   },
 });
 
+// const database = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD, {
+//   dialect: 'mysql',
+//   host: 'remotemysql.com',
+//   define: {
+//     timestamps: false,
+//   },
+// });
+//
 module.exports = { database }
